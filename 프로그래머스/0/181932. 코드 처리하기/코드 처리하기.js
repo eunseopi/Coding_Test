c0acc09fb83772df933c5f3b6d95ecaf84d3ed36
eunseopi <117704586+eunseopi@users.noreply.github.com>
2025-03-11 21:40:09 +0900

    [level 0] Title: 코드 처리하기, Time: 44.92 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,14 @@
+function solution(code) {
+    let result = ""
+    let mode = 0;
+    
+    for (let i=0; i<code.length; i++) {
+        if(mode===0){
+            code[i] === '1' ? (mode=1):(i%2===0 && (result += code[i]))
+        }else {
+            code[i] === '1' ? (mode=0):(i%2!==0 && (result += code[i]))
+        }
+    }
+    
+    return result.length>0? result: "EMPTY";
+}
