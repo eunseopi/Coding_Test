39afebb65df860559a26c78201807a5352c86b27
Hushed_Mind <117704586+eunseopi@users.noreply.github.com>
2025-03-21 18:23:05 +0900

    [level 3] Title: N으로 표현, Time: 21.12 ms, Memory: 39.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -0,0 +1,23 @@
+function solution(N, number) {
+    if (N === number) return 1;
+    
+    const memo = Array.from({length: 9}, () => new Set());
+    
+    for(let i=1; i<=8; i++) {
+        memo[i].add(Number(String(N).repeat(i)));
+        for (let j=1; j < i; j++) {
+            for(const a of memo[j]){
+                for(const b of memo[i-j]){
+                    memo[i].add(a+b);
+                    memo[i].add(a-b);
+                    memo[i].add(a*b);
+                    if(b!==0) memo[i].add(Math.floor(a/b));
+                }
+            }
+        }
+        
+        if(memo[i].has(number)) return i;
+    }
+    
+    return -1;
+}
